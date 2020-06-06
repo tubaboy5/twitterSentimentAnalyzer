0adae35489c33e0c4add603c3bf0f840bea0b8b5
Justin Hartmann <tubaboy5@gmail.com>
2020-06-06 16:44:43 -0500

    Checking Twitter API

--- index.js
+++ index.js
@@ -2,27 +2,31 @@ require("dotenv").config();
 
 const Twitter = require("twitter-lite");
 
-const user = new Twitter({
-  consumer_key: process.env.API_KEY,
-  consumer_secret: process.env.API_SECRET_KEY,
-});
-
-// Wrap the following code in an async function that is called
-// immediately so that we can use "await" statements.
 (async function () {
-  try {
-    // Retrieve the bearer token from twitter.
-    const response = await user.getBearerToken();
-    console.log(
-      `Got the following Bearer token from Twitter: ${response.access_token}`
-    );
+  const user = new Twitter({
+    consumer_key: process.env.API_KEY,
+    consumer_secret: process.env.API_SECRET_KEY,
+  });
 
-    // Construct our API client with the bearer token.
+  try {
+    let response = await user.getBearerToken();
     const app = new Twitter({
       bearer_token: response.access_token,
     });
+
+    // Search for recent tweets from the twitter API
+    response = await app.get(`/search/tweets`, {
+      q: "Lionel Messi", // The search term
+      lang: "en", // Let's only get English tweets
+      count: 100, // Limit the results to 100 tweets
+    });
+
+    // Loop over all the tweets and print the text
+    for (tweet of response.statuses) {
+      console.dir(tweet.text);
+    }
   } catch (e) {
-    console.log("There was an error calling the Twitter API.");
+    console.log("There was an error calling the Twitter API");
     console.dir(e);
   }
 })();
